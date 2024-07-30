e67e7871a6c13acf9959a6cc2b750164bf081726
AbdulHaseb9 <haseebsheikh2380@gmail.com>
2024-07-30 12:50:42 -0700

    add projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,27 +1,49 @@
-import hase from "/portfolio1.jpg";
+import dyssey_cosmoc from '/projects/dyssey-cosmoc.png';
+import logo_ipsum from '/projects/logo-ipsum.png';
+import hoo_bank from '/projects/hoo-bank.png';
+import wise_market from '/projects/wise-market.png';
+import abothers from '../../public/projects/abrothers.png';
+
+
 export const mywork = [
   {
-    name: "Modern Dashboard",
-    image: hase,
+    name: "Abrothers",
+    image: abothers,
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam voluptate repudiandae velitconsectetur adipisicing elit. Numquam voluptate repudiandae velit praesentium nam ab.",
+    technologies: ['React + Vite', 'Tailwindcss', 'Redux Toolkit', 'MongoDb'],
+    type: 'fullstack',
   },
   {
-    name: "Modern Dashboard",
-    image: hase,
+    name: "Wise Market",
+    image: wise_market,
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam voluptate repudiandae velitconsectetur adipisicing elit. Numquam voluptate repudiandae velit praesentium nam ab.",
+    technologies: ['React + Vite', 'Tailwindcss', 'Redux Toolkit', 'MongoDb'],
+    type: 'react',
   },
   {
-    name: "Modern Dashboard",
-    image: hase,
+    name: "Dyssey Cosmoc",
+    image: dyssey_cosmoc,
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam voluptate repudiandae velitconsectetur adipisicing elit. Numquam voluptate repudiandae velit praesentium nam ab.",
+    technologies: ['React + Vite', 'Tailwindcss', 'Redux Toolkit', 'MongoDb'],
+    type: 'react',
   },
   {
-    name: "Modern Dashboard",
-    image: hase,
+    name: "Hoo Bank",
+    image: hoo_bank,
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam voluptate repudiandae velitconsectetur adipisicing elit. Numquam voluptate repudiandae velit praesentium nam ab.",
+    technologies: ['React + Vite', 'Tailwindcss', 'Redux Toolkit', 'MongoDb'],
+    type: 'react',
+  },
+  {
+    name: "Logo Ipsum",
+    image: logo_ipsum,
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam voluptate repudiandae velitconsectetur adipisicing elit. Numquam voluptate repudiandae velit praesentium nam ab.",
+    technologies: ['React + Vite', 'Tailwindcss', 'Redux Toolkit', 'MongoDb'],
+    type: 'react',
   },
 ];
