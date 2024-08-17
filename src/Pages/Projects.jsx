68167f48245c506d484312a0f1e290f60d9a2df7
AbdulHaseb9 <haseebsheikh2380@gmail.com>
2024-08-17 15:37:45 -0700

    add projects live links

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { mywork } from "../components/Work";
+import { Link } from "react-router-dom";
 
 export default function Projects() {
-  const [projects, setProjects] = useState('all')
+  const [projects, setProjects] = useState("all");
   return (
     <div className=" w-full bg-secondary py-20 px-5 md:px-20">
       <h3 className="text-3xl md:text-4xl text-center font-semibold font-roman [word-spacing:-8px]">
@@ -11,92 +12,120 @@ export default function Projects() {
       {/* Filter Btns */}
       <div className="flex justify-center flex-wrap my-5 gap-x-5 gap-y-3">
         <button
-          onClick={() => setProjects('all')}
-          className={`rounded-md border-primary px-4 py-2 border-2 ${projects == 'all' ? `bg-transparent text-primary ` : `bg-primary text-white`}`}
+          onClick={() => setProjects("all")}
+          className={`rounded-md border-primary px-4 py-2 border-2 ${
+            projects == "all"
+              ? `bg-transparent text-primary `
+              : `bg-primary text-white`
+          }`}
         >
           All
         </button>
         <button
-          onClick={() => setProjects('javascript')}
-          className={`rounded-md border-primary px-4 py-2 border-2 ${projects == 'javascript' ? `bg-transparent text-primary ` : `bg-primary text-white`}`}
+          onClick={() => setProjects("javascript")}
+          className={`rounded-md border-primary px-4 py-2 border-2 ${
+            projects == "javascript"
+              ? `bg-transparent text-primary `
+              : `bg-primary text-white`
+          }`}
         >
           Javascript
         </button>
         <button
-          onClick={() => setProjects('react')}
-          className={`rounded-md border-primary px-4 py-2 border-2 ${projects == 'react' ? `bg-transparent text-primary ` : `bg-primary text-white`}`}
+          onClick={() => setProjects("react")}
+          className={`rounded-md border-primary px-4 py-2 border-2 ${
+            projects == "react"
+              ? `bg-transparent text-primary `
+              : `bg-primary text-white`
+          }`}
         >
           React
         </button>
         <button
-          onClick={() => setProjects('fullstack')}
-          className={`px-4 py-2 rounded-md border-primary border-2 ${projects == 'fullstack' ? `bg-transparent text-primary ` : `bg-primary text-white`}`}
+          onClick={() => setProjects("fullstack")}
+          className={`px-4 py-2 rounded-md border-primary border-2 ${
+            projects == "fullstack"
+              ? `bg-transparent text-primary `
+              : `bg-primary text-white`
+          }`}
         >
           Full Stack Apps
         </button>
       </div>
       {/* Projects */}
       <div className="grid grid-cols-1 lg:grid-cols-2 place-items-center gap-y-5 my-6">
-        {projects == 'all' ?
+        {projects == "all" ? (
           mywork.map((item, index) => (
-            <div
-              className="md:h-56 lg:h-72 flex flex-col rounded-lg bg-white/55 md:max-w-xl md:flex-row"
-              key={index}>
-              <img
-                className="h-48 w-full rounded-t-lg object-top object-cover md:h-auto md:w-4/12 md:!rounded-none md:!rounded-s-lg"
-                src={item.image}
-                alt={item.name} />
-              <div className="flex flex-col justify-start p-6 md:w-8/12">
-                <h5 className="mb-2 text-xl font-medium text-black">{item.name}</h5>
-                <p className="mb-4 text-sm text-gray text-justify">{item.description}</p>
-                <div className="flex flex-wrap">
-                  {item.technologies.map((tech, techIndex) => (
-                    <span
-                      key={techIndex}
-                      className="text-xs text-primary m-1 rounded-full"
-                    // className="text-xs bg-primary text-secondary p-1 m-1 rounded-full"
-                    >
-                      {tech + ' ,'}
-                    </span>
-                  ))}
+            <Link to={item.href} target="_blank">
+              <div
+                className="md:h-56 lg:h-72 flex flex-col rounded-lg bg-white/55 md:max-w-xl md:flex-row"
+                key={index}
+              >
+                <img
+                  className="h-48 w-full rounded-t-lg object-top object-cover md:h-auto md:w-4/12 md:!rounded-none md:!rounded-s-lg"
+                  src={item.image}
+                  alt={item.name}
+                />
+                <div className="flex flex-col justify-start p-6 md:w-8/12">
+                  <h5 className="mb-2 text-xl font-medium text-black">
+                    {item.name}
+                  </h5>
+                  <p className="mb-4 text-sm text-gray text-justify">
+                    {item.description}
+                  </p>
+                  <div className="flex flex-wrap">
+                    {item.technologies.map((tech, techIndex) => (
+                      <span
+                        key={techIndex}
+                        className="text-xs text-primary m-1 rounded-full"
+                        // className="text-xs bg-primary text-secondary p-1 m-1 rounded-full"
+                      >
+                        {tech + " ,"}
+                      </span>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
+            </Link>
           ))
-          : mywork
-            .filter(item => item.type == projects).length > 0 ?
-            mywork
-              .filter(item => item.type == projects)
-              .map((item, index) => {
-                return (
-                  <div
-                    className="md:h-56 lg:h-64 flex flex-col rounded-lg bg-white dark:text-white md:max-w-xl md:flex-row"
-                    key={index} >
-                    <img
-                      className="h-48 w-full rounded-t-lg object-top object-cover md:h-auto md:w-4/12 md:!rounded-none md:!rounded-s-lg"
-                      src={item.image}
-                      alt={item.name} />
-                    <div className="flex flex-col justify-start p-6 md:w-8/12">
-                      <h5 className="mb-2 text-xl font-medium text-black">{item.name}</h5>
-                      <p className="mb-4 text-sm text-gray">{item.description}</p>
-                      <div className="flex flex-wrap">
-                        {item.technologies.map((tech, techIndex) => (
-                          <span
-                            key={techIndex}
-                            className="text-xs text-gray m-1 rounded-full"
+        ) : mywork.filter((item) => item.type == projects).length > 0 ? (
+          mywork
+            .filter((item) => item.type == projects)
+            .map((item, index) => (
+              <Link to={item.href} target="_blank">
+                <div
+                  className="md:h-56 lg:h-72 flex flex-col rounded-lg bg-white/55 md:max-w-xl md:flex-row"
+                  key={index}
+                >
+                  <img
+                    className="h-48 w-full rounded-t-lg object-top object-cover md:h-auto md:w-4/12 md:!rounded-none md:!rounded-s-lg"
+                    src={item.image}
+                    alt={item.name}
+                  />
+                  <div className="flex flex-col justify-start p-6 md:w-8/12">
+                    <h5 className="mb-2 text-xl font-medium text-black">
+                      {item.name}
+                    </h5>
+                    <p className="mb-4 text-sm text-gray text-justify">{item.description}</p>
+                    <div className="flex flex-wrap">
+                      {item.technologies.map((tech, techIndex) => (
+                        <span
+                          key={techIndex}
+                          className="text-xs text-gray m-1 rounded-full"
                           // className="text-xs bg-primary text-secondary p-1 m-1 rounded-full"
-                          >
-                            {tech + ' ,'}
-                          </span>
-                        ))}
-                      </div>
+                        >
+                          {tech + " ,"}
+                        </span>
+                      ))}
                     </div>
                   </div>
-                )
-              })
-            : <div className="col-span-2">No project Found</div>
-        }
+                </div>
+              </Link>
+            ))
+        ) : (
+          <div className="col-span-2">No project Found</div>
+        )}
       </div>
-    </div >
+    </div>
   );
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,8 @@
-import dyssey_cosmoc from '/projects/dyssey-cosmoc.png';
-import logo_ipsum from '/projects/logo-ipsum.png';
-import hoo_bank from '/projects/hoo-bank.png';
-import wise_market from '/projects/wise-market.png';
-import abothers from '../../public/projects/abrothers.png';
-
+import dyssey_cosmoc from "/projects/dyssey-cosmoc.png";
+import logo_ipsum from "/projects/logo-ipsum.png";
+import hoo_bank from "/projects/hoo-bank.png";
+import wise_market from "/projects/wise-market.png";
+import abothers from "../../public/projects/abrothers.png";
 
 export const mywork = [
   {
@@ -11,39 +10,54 @@ export const mywork = [
     image: abothers,
     description:
       "Developed a fast, modern e-commerce website using React with Vite, TailwindCSS, and Redux Toolkit. The site includes user authentication, product listings, a shopping cart, and a checkout process, enhanced with React Router DOM for navigation and React Hot Toast for notifications.",
-    technologies: ['React + Vite', 'Tailwindcss', 'Redux Toolkit', 'MongoDb', 'Express JS', 'Node JS', 'bcrypt', 'jwt', 'npm packages',],
-    type: 'fullstack',
+    technologies: [
+      "React + Vite",
+      "Tailwindcss",
+      "Redux Toolkit",
+      "MongoDb",
+      "Express JS",
+      "Node JS",
+      "bcrypt",
+      "jwt",
+      "npm packages",
+    ],
+    type: "fullstack",
+    href: "https://abrothers.vercel.app/",
   },
   {
     name: "Wise Market",
     image: wise_market,
     description:
       "Created a responsive homepage clone of a popular e-commerce site using React with Vite, TailwindCSS, and Redux Toolkit. Features include a featured products section and user-friendly navigation, enhanced with React Router DOM.",
-    technologies: ['React + Vite', 'Tailwindcss', 'Redux Toolkit', 'MongoDb'],
-    type: 'react',
+    technologies: ["React + Vite", "Tailwindcss", "Redux Toolkit", "MongoDb"],
+    type: "react",
+    href: "https://wisemarketclone.web.app/",
   },
   {
     name: "Dyssey Cosmoc",
     image: dyssey_cosmoc,
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam voluptate repudiandae velitconsectetur adipisicing elit. Numquam voluptate repudiandae velit praesentium nam ab.",
-    technologies: ['React + Vite', 'Tailwindcss', 'Redux Toolkit', 'MongoDb'],
-    type: 'react',
+    technologies: ["React + Vite", "Tailwindcss", "Redux Toolkit", "MongoDb"],
+    type: "react",
+    href: "https://dyssey-cosmoc.web.app/",
   },
   {
     name: "Hoo Bank",
     image: hoo_bank,
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam voluptate repudiandae velitconsectetur adipisicing elit. Numquam voluptate repudiandae velit praesentium nam ab.",
-    technologies: ['React + Vite', 'Tailwindcss', 'Redux Toolkit', 'MongoDb'],
-    type: 'react',
+    technologies: ["React + Vite", "Tailwindcss", "Redux Toolkit", "MongoDb"],
+    type: "react",
+    href: "https://projecthoobank.web.app/",
   },
   {
     name: "Logo Ipsum",
     image: logo_ipsum,
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam voluptate repudiandae velitconsectetur adipisicing elit. Numquam voluptate repudiandae velit praesentium nam ab.",
-    technologies: ['React + Vite', 'Tailwindcss', 'Redux Toolkit', 'MongoDb'],
-    type: 'react',
+    technologies: ["React + Vite", "Tailwindcss", "Redux Toolkit", "MongoDb"],
+    type: "react",
+    href: "https://logoipsumweb.netlify.app/",
   },
 ];
